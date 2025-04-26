2921f9f7029476adfc7f2be07ffb2b8bb261cb82
walid6789 <eulmiwalid02@gmail.com>
2025-04-26 11:51:09 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+x = 'walid'
+document.writeln(x);
+alert('hello world');
+window.alert('hello world alert');
+
+
+
+
+
+note1 = 10
+note2 = 13
+note3 = 13 
+note4 = 18
+note5 = 17
+note6 = 19
+note7 = 11
+note8 = 13
+note9 = 15
+note10 = 12
+coefficient = 10
+moyenne = (10 + 13 + 13 + 18 + 17 + 19 + 19 + 11 + 13 + 15 + 12) / 10; 
+document.write( "La moyenne est de moh est : " + moyenne);
